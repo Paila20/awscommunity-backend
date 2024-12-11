10cf1532e522f751deffc901784ca30c28fdd7a4
paila20 <rajarajeswar169@gmail.com>
2024-12-11 17:59:40 +0530

    updated auth

--- Middlewares/Auth.js
+++ Middlewares/Auth.js
@@ -18,29 +18,3 @@ const ensureAuthenticated = (req, res, next) => {
 
 module.exports = ensureAuthenticated;
 
-// const jwt = require('jsonwebtoken');
-
-// const ensureAuthenticated = (req, res, next) => {
-//     const authHeader = req.headers['authorization'];
-//     console.log('Authorization Header:', authHeader);
-//     if (!authHeader) {
-//         return res.status(403).json({ message: 'Unauthorized, JWT token is required' });
-//     }
-
-//     // Extract the token by removing "Bearer " from the Authorization header
-//     const token = authHeader.split(' ')[1];
-
-//     if (!token) {
-//         return res.status(403).json({ message: 'Unauthorized, JWT token is required' });
-//     }
-
-//     try {
-//         const decoded = jwt.verify(token, process.env.JWT_SECRET);  // Ensure the JWT_SECRET matches your signing key
-//         req.user = decoded;  // Store the decoded token in the request object
-//         next();  // Pass control to the next middleware or route handler
-//     } catch (err) {
-//         return res.status(403).json({ message: 'Unauthorized, JWT token is wrong or expired' });
-//     }
-// };
-
-// module.exports = ensureAuthenticated;
