3c675c9f3359dbcf530deee4d70324a217220f2c
paila20 <rajarajeswar169@gmail.com>
2024-12-16 17:49:51 +0530

    updated index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const ProductRouter = require('./Routes/ProductRouter');
 
 const blogRoutes = require('./Routes/BlogRouter');
 const path = require('path');
+const fs = require('fs');
 
 require('dotenv').config();
 require('./Models/db');
@@ -15,7 +16,13 @@ const PORT = process.env.PORT || 5000;
 app.get('/ping', (req, res) => {
     res.send('PONG');
 });
-
+const uploadDir = path.join(__dirname, 'uploads');
+if (!fs.existsSync(uploadDir)) {
+    fs.mkdirSync(uploadDir, { recursive: true }); // Ensure that the folder is created
+    console.log('Uploads directory created.');
+} else {
+    console.log('Uploads directory already exists.');
+}
 
 
 app.use(express.json());
@@ -33,7 +40,7 @@ app.use(cors({
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
 
-app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
+app.use('/uploads', express.static(uploadDir)); 
 app.use('/api/blogs', blogRoutes);
 
 app.listen(PORT, () => {
