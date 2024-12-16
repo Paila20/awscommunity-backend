915935337003ab749dc397320cb051fa285594d0
paila20 <rajarajeswar169@gmail.com>
2024-12-16 11:11:36 +0530

    updated index.js

--- index.js
+++ index.js
@@ -4,8 +4,7 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const AuthRouter = require('./Routes/AuthRouter');
 const ProductRouter = require('./Routes/ProductRouter');
-// const ExpenseRouter = require('./Routes/ExpenseRouter');
-// const ensureAuthenticated = require('./Middlewares/Auth');
+
 const blogRoutes = require('./Routes/BlogRouter');
 const path = require('path');
 
@@ -17,10 +16,7 @@ app.get('/ping', (req, res) => {
     res.send('PONG');
 });
 
-// app.use(express.json({ limit: '10mb' })); 
-// app.use(express.urlencoded({ limit: '10mb', extended: true })); 
 
-// app.use(bodyParser.json());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
@@ -31,7 +27,7 @@ app.use(cors());
 
 app.use('/auth', AuthRouter);
 app.use('/products', ProductRouter);
-// app.use('/expenses', ensureAuthenticated, ExpenseRouter)
+
 app.use('/uploads', express.static(path.join(__dirname, 'uploads'))); 
 app.use('/api/blogs', blogRoutes);
 
