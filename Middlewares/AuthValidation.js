15ac8a1b857b4ba17dd57c1103d8e619853f6ba7
paila20 <rajarajeswar169@gmail.com>
2024-12-16 12:18:15 +0530

    updated auth validations

--- Middlewares/AuthValidation.js
+++ Middlewares/AuthValidation.js
@@ -1,10 +1,11 @@
 const Joi = require('joi');
 
+const passwordRegex = /^(?=.*[A-Za-z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,100}$/;
 const signupValidation = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(3).max(100).required(),
         email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
+        password: Joi.string().min(8).pattern(passwordRegex).required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
@@ -16,7 +17,7 @@ const signupValidation = (req, res, next) => {
 const loginValidation = (req, res, next) => {
     const schema = Joi.object({
         email: Joi.string().email().required(),
-        password: Joi.string().min(4).max(100).required()
+        password: Joi.string().min(8).pattern(passwordRegex).required()
     });
     const { error } = schema.validate(req.body);
     if (error) {
