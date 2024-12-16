5b57956ef87d9370ec0a19f0f292b23668eeda91
paila20 <rajarajeswar169@gmail.com>
2024-12-16 12:55:29 +0530

    added email regex

--- Middlewares/AuthValidation.js
+++ Middlewares/AuthValidation.js
@@ -4,7 +4,7 @@ const passwordRegex =  /^(?=.*[0-9])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
 const signupValidation = (req, res, next) => {
     const schema = Joi.object({
         name: Joi.string().min(3).max(100).required(),
-        email: Joi.string().email().required(),
+        email: Joi.string().email().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
         password: Joi.string().min(8).pattern(passwordRegex).required()
     });
     const { error } = schema.validate(req.body);
@@ -16,7 +16,7 @@ const signupValidation = (req, res, next) => {
 }
 const loginValidation = (req, res, next) => {
     const schema = Joi.object({
-        email: Joi.string().email().required(),
+        email: Joi.string().email().pattern(/^[^\s@]+@[^\s@]+\.[^\s@]+$/).required(),
         password: Joi.string().min(8).pattern(passwordRegex).required()
     });
     const { error } = schema.validate(req.body);
